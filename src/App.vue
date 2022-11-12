2ca8a8b1bda889bd1d360789f42f3c811a027fcb
mufeng2333 <2441260435@qq.com>
2022-11-12 10:40:40 +0800

    增加暗黑模式

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,59 @@
           variant="icon"
           @click="toggle"
         ></v-btn>
-
+        <v-btn
+          v-if="theme.global.current.value.dark"
+          icon="mdi-brightness-5"
+          variant="icon"
+          @click="toggleTheme()"
+        ></v-btn>
+        <v-btn
+          v-else
+          icon="mdi-brightness-4"
+          variant="icon"
+          @click="toggleTheme()"
+        ></v-btn>
+        <v-btn
+          icon="mdi-github"
+          variant="icon"
+          href="https://github.com/mufeng233/CEECountdown"
+          target="_BLANK"
+        >
+        </v-btn>
+        <v-dialog
+          v-model="dialog"
+          fullscreen
+          :scrim="false"
+          transition="dialog-bottom-transition"
+        >
+          <template v-slot:activator="{ props }">
+            <v-btn icon="mdi-cog" v-bind="props" variant="icon"></v-btn>
+          </template>
+          <v-card>
+            <v-toolbar dark color="primary" elevation="5">
+              <v-btn icon dark @click="dialog = false">
+                <v-icon>mdi-close</v-icon>
+              </v-btn>
+              <v-toolbar-title>设置</v-toolbar-title>
+              <v-spacer></v-spacer>
+              <v-toolbar-items>
+                <v-btn variant="text" @click="dialog = false"> 保存 </v-btn>
+              </v-toolbar-items>
+            </v-toolbar>
+            <v-container>
+              <div class="text-h2">开发中...</div>
+              <v-slider
+                v-model="fontSize"
+                prepend-icon="mdi-font"
+                color="primary"
+              >
+                <template v-slot:prepend>
+                  <div class="text-h6">字体大小</div>
+                </template></v-slider
+              >
+            </v-container>
+          </v-card>
+        </v-dialog>
         <v-spacer></v-spacer>
         <v-icon icon="mdi-wifi-strength-4"></v-icon>
         <v-icon icon="mdi-signal" class="ml-2"></v-icon>
@@ -26,8 +78,9 @@
       </v-system-bar>
       <v-main>
         <Container />
-      </v-main> </v-app
-  ></UseFullscreen>
+      </v-main>
+    </v-app>
+  </UseFullscreen>
 </template>
 
 <script setup>
@@ -35,6 +88,14 @@ import moment from "moment";
 import { ref } from "vue";
 import Container from "@/components/Container.vue";
 import { UseFullscreen } from "@vueuse/components";
+import { useTheme } from "vuetify";
+const theme = useTheme();
+const toggleTheme = () => {
+  theme.global.name.value = theme.global.current.value.dark ? "light" : "dark";
+};
+
+let dialog = ref(false);
+let fontSize = ref(0);
 
 let now = ref(new Date());
 setInterval(() => {
