ef3885b3a3aa51107970c404130ce04a736b4289
mufeng2333 <2441260435@qq.com>
2022-11-12 10:40:48 +0800

    update

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,22 +5,32 @@
  */
 
 // Styles
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
 
 // Composables
-import { createVuetify } from 'vuetify'
+import { createVuetify } from "vuetify";
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
+    defaultTheme: "light",
     themes: {
       light: {
+        // dark:
+        // localStorage.getItem("vueuse-color-scheme") === "dark" ? true : false,
         colors: {
-          primary: '#1867C0',
-          secondary: '#5CBBF6',
+          primary: "#1867C0",
+          secondary: "#5CBBF6",
         },
       },
     },
+    dark: {
+      dark: true,
+      colors: {
+        primary: "#1867C0",
+        secondary: "#5CBBF6",
+      },
+    },
   },
-})
+});
